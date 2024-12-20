58e13bec4cabbfd999814e0dbaa3b6390c5d9986
Aremchik <artema.1605@gmail.com>
2024-12-21 02:51:18 +0300

    wip

--- auth_service/app/api/model/model.py
+++ auth_service/app/api/model/model.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, String, Integer, DateTime, Boolean
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
-from api.database.database import Base
+from app.api.database.database import Base
 
 class UserModel(Base):
     __tablename__ = "users"
