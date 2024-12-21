6767d21d87aadbedefafd31e459df47c1b21b1f0
Aremchik <artema.1605@gmail.com>
2024-12-21 10:29:18 +0300

    wip

--- team_service/app/api/database/database.py
+++ team_service/app/api/database/database.py
@@ -1,14 +1,20 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import declarative_base
 
-
-DATABASE_URL = "sqlite+aiosqlite:////home/root/dev/OperationBackend/operation.sqlite"
+# Измените URL базы данных на PostgreSQL
+DATABASE_URL = "postgresql+asyncpg://postgres:password@postgres:5432/postgres"
 
 Base = declarative_base()
 
+# Создаем движок и сессию для работы с базой данных
 engine = create_async_engine(DATABASE_URL, future=True, echo=True)
 SessionLocal = async_sessionmaker(bind=engine, expire_on_commit=False)
 
 async def get_db():
     async with SessionLocal() as session:
         yield session
+
+# Функция для инициализации базы данных
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
--- user_service/app/api/database/database.py
+++ user_service/app/api/database/database.py
@@ -1,14 +1,12 @@
-# Убедитесь, что SQLAlchemy версии 1.4 или выше
-from sqlalchemy import create_engine
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import declarative_base
 
-
-DATABASE_URL = "sqlite+aiosqlite:////home/root/dev/OperationBackend/operation.sqlite"
+# Измените URL базы данных на PostgreSQL
+DATABASE_URL = "postgresql+asyncpg://postgres:password@postgres:5432/postgres"
 
 Base = declarative_base()
 
-# Используйте create_async_engine для асинхронных операций
+# Создаем движок и сессию для работы с базой данных
 engine = create_async_engine(DATABASE_URL, future=True, echo=True)
 SessionLocal = async_sessionmaker(bind=engine, expire_on_commit=False)
 
@@ -16,9 +14,7 @@ async def get_db():
     async with SessionLocal() as session:
         yield session
 
-# Функция для создания таблиц
+# Функция для инициализации базы данных
 async def init_db():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-
-# Запускаем функцию инициализации базы данных
