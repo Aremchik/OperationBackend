bf4f65574207428bb187f405a2d6fd9d87857078
Aremchik <artema.1605@gmail.com>
2024-12-23 00:06:06 +0300

    wip

--- auth_service/app/api/model/model.py
+++ auth_service/app/api/model/model.py
@@ -39,3 +39,4 @@ class TeamMemberModel(Base):
 
     team_id = Column(UUID(as_uuid=True), ForeignKey("teams.id", ondelete="CASCADE"), primary_key=True)
     user_id = Column(UUID(as_uuid=True), ForeignKey("users.id", ondelete="CASCADE"), primary_key=True)
+
