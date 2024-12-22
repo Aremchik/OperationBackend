50e919d04fac8775088c71647f392fc7f3e6e5f3
Aremchik <artema.1605@gmail.com>
2024-12-22 23:55:58 +0300

    wip

--- auth_service/app/api/schemas.py
+++ auth_service/app/api/schemas.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel
 from datetime import datetime
 from typing import Optional
 from uuid import UUID as UUIDType, uuid4
-from sqlalchemy import Column, String, Integer, DateTime
+from datetime import datetime as DatetimeType
 
 class UserSchema(BaseModel):
     id: UUIDType
@@ -10,17 +10,19 @@ class UserSchema(BaseModel):
     name: str
     email: str
     status: int
-    birthday: Optional[DateTime]
-    created_at: DateTime
+    birthday: Optional[DatetimeType]
+    created_at: DatetimeType
     team_id: Optional[UUIDType]
 
     class Config:
         orm_mode = True
+        arbitrary_types_allowed = True
 
 class TeamSchema(BaseModel):
     id: UUIDType
     name: str
-    created: DateTime
+    created: DatetimeType
 
     class Config:
         orm_mode = True
+        arbitrary_types_allowed = True
